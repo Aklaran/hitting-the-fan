a75569db0ea4b949860ff4ca05be180722123adf
Bo Tembunkiart <Aklaran@users.noreply.github.com>
2024-05-05 10:13:35 -0400

    feat: Init Express server

--- server.ts
+++ server.ts
@@ -1,1 +1,12 @@
-console.log("Hello, Saturn!");
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, Saturn!");
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port 3000");
+});
