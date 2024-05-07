ed7976ac598b805ade336cf6b62a9faa521d11c1
Bo Tembunkiart <Aklaran@users.noreply.github.com>
2024-05-06 23:17:58 -0400

    build: Set up auto format w/ prettier-eslint
    Trivago import sorting not working yet

---
+++ .prettierrc.js
@@ -0,0 +1,21 @@
+module.exports =  {
+  semi: false,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+
+  // TODO: Get import sorter plugin working
+  // Import Sorting Config
+  // importOrder: [
+  //   '^@/lib/(.*)$',
+  //   '^@/(.*)$',
+  //   '^[./]',
+  // ],
+  // importOrderSeparation: true,
+  // importOrderSortSpecifiers: true,
+  // importOrderParserPlugins: ['typescript', 'decorators-legacy'],
+
+  // Plugins
+  // plugins: ['@trivago/prettier-plugin-sort-imports'],
+}
