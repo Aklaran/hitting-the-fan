74804ff31d2b3c63cf992e9b9204c82429c1a35a
Bo Tembunkiart <Aklaran@users.noreply.github.com>
2024-05-07 07:55:22 -0400

    feat: Add GET/:id and DELETE/:id Flashcards routes

--- routes/flashcards.ts
+++ routes/flashcards.ts
@@ -24,6 +24,17 @@ flashcardsRoute.get('/', (_req, res) => {
   res.json(fakeFlashcards)
 })
 
+// TODO: Convert route param to GUID
+flashcardsRoute.get('/:id(\\d+)', (req, res) => {
+  const id = Number(req.params.id)
+
+  const flashcard = fakeFlashcards.find((flashcard) => flashcard.id === id)
+
+  if (!flashcard) {
+    res.send(StatusCodes.NOT_FOUND)
+  } else {
+    res.status(StatusCodes.OK).json(flashcard)
+  }
 })
 
 flashcardsRoute.post('/', validateData(createFlashcardSchema), (req, res) => {
@@ -38,6 +49,21 @@ flashcardsRoute.post('/', validateData(createFlashcardSchema), (req, res) => {
   fakeFlashcards.push(newFlashcard)
 
   res.status(StatusCodes.CREATED).json(newFlashcard)
+})
+
+// TODO: Convert route param to GUID
+flashcardsRoute.delete('/:id(\\d+)', (req, res) => {
+  const id = Number(req.params.id)
+
+  const index = fakeFlashcards.findIndex((flashcard) => flashcard.id === id)
+
+  if (index === -1) {
+    res.send(StatusCodes.NOT_FOUND)
+  } else {
+    const deletedFlashcard = fakeFlashcards.splice(index, 1)[0]
+
+    res.status(StatusCodes.OK).json(deletedFlashcard)
+  }
 })
 
 export default flashcardsRoute
