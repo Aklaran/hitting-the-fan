2754ce79508c4ab34948ba4315bf36f41dab2e7c
Bo Tembunkiart <Aklaran@users.noreply.github.com>
2024-05-07 20:34:54 -0400

    build: Simplify nested estlintrcs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
+  root: true,
   env: {
     node: true,
   },
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
   ],
 };
--- app/frontend/.eslintrc.cjs
+++ app/frontend/.eslintrc.cjs
@@ -1,13 +1,10 @@
 module.exports = {
-  root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    '../../.eslintrc.js',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
+  ignorePatterns: ['dist'],
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
