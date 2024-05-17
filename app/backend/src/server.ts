e393939e56f66be4ba1dccf9de7dc4930f11ed8e
Bo Tembunkiart <Aklaran@users.noreply.github.com>
2024-05-16 22:04:23 -0400

    chore: Log port more specifically

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -72,5 +72,5 @@ app.get('*', (_req, res) => {
 // -------------------------------
 
 app.listen(port, () => {
-  console.log('Server is running on port 3000')
+  console.log(`Server is running on port ${port}`)
 })
