4822895c4adccf1ca7f425c587338e33d6b9ca86
Bo Tembunkiart <Aklaran@users.noreply.github.com>
2024-05-19 16:59:04 -0400

    feat: Make improvements to new flashcard form
    - Navigate to flashcard index on successful submission
    - Validate fields to require both on FE and BE
    - Only show submit button if form can be submitted

--- app/frontend/src/routes/flashcards/new.tsx
+++ app/frontend/src/routes/flashcards/new.tsx
@@ -3,7 +3,9 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { trpc } from '@/lib/trpc'
 import { useForm } from '@tanstack/react-form'
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useNavigate } from '@tanstack/react-router'
+import { zodValidator } from '@tanstack/zod-form-adapter'
+import { z } from 'zod'
 
 export const Route = createFileRoute('/flashcards/new')({
   component: FlashcardForm,
@@ -12,17 +14,23 @@ export const Route = createFileRoute('/flashcards/new')({
 function FlashcardForm() {
   const mutation = trpc.flashcard.create.useMutation()
 
+  const navigate = useNavigate()
+
   const form = useForm({
     defaultValues: {
       question: '',
       answer: '',
     },
+    validatorAdapter: zodValidator,
     onSubmit: async (values) => {
       const { question, answer } = values.value
+
       mutation.mutate({
         question,
         answer,
       })
+
+      navigate({ to: '/flashcards' })
     },
   })
 
@@ -36,40 +44,68 @@ function FlashcardForm() {
         }}
       >
         <div>
+          {/* REFACTOR: Componentize form fields */}
           <form.Field
             name="question"
+            validators={{
+              onChange: z.string().min(1, 'Question is required.'),
+            }}
             children={(field) => (
               <>
-                <Label htmlFor="question">Question</Label>
+                <Label htmlFor={field.name}>Question</Label>
                 <Input
-                  id="question"
-                  name="question"
+                  id={field.name}
+                  name={field.name}
                   type="text"
                   value={field.state.value}
                   onBlur={field.handleBlur}
                   onChange={(e) => field.handleChange(e.target.value)}
                 />
+                {field.state.meta.touchedErrors ? (
+                  <div className="text-red-500">
+                    {field.state.meta.touchedErrors.map((error) =>
+                      error?.toString(),
+                    )}
+                  </div>
+                ) : null}
               </>
             )}
           />
           <form.Field
             name="answer"
+            validators={{
+              onChange: z.string().min(1, 'Answer is required.'),
+            }}
             children={(field) => (
               <>
-                <Label htmlFor="answer">Answer</Label>
+                <Label htmlFor={field.name}>Answer</Label>
                 <Input
-                  id="answer"
-                  name="answer"
+                  id={field.name}
+                  name={field.name}
                   type="text"
                   value={field.state.value}
                   onBlur={field.handleBlur}
                   onChange={(e) => field.handleChange(e.target.value)}
                 />
+                {field.state.meta.touchedErrors ? (
+                  <div className="text-red-500">
+                    {field.state.meta.touchedErrors.map((error) =>
+                      error?.toString(),
+                    )}
+                  </div>
+                ) : null}
               </>
             )}
           />
         </div>
-        <Button type="submit">Submit</Button>
+        <form.Subscribe
+          selector={(state) => [state.canSubmit, state.isSubmitting]}
+          children={([canSubmit, isSubmitting]) => (
+            <Button type="submit" disabled={!canSubmit}>
+              {isSubmitting ? '...' : 'Submit'}
+            </Button>
+          )}
+        />
       </form>
     </div>
   )
--- app/shared/types/flashcard.ts
+++ app/shared/types/flashcard.ts
@@ -2,8 +2,12 @@ import { z } from 'zod'
 
 const flashcardSchema = z.object({
   id: z.number().int().positive().min(1),
-  question: z.string({ required_error: 'Question is required.' }),
-  answer: z.string({ required_error: 'Answer is required.' }),
+  question: z.string({ required_error: 'Question is required.' }).min(1, {
+    message: 'Question is required.',
+  }),
+  answer: z.string({ required_error: 'Answer is required.' }).min(1, {
+    message: 'Answer is required.',
+  }),
 })
 
 export const createFlashcardSchema = flashcardSchema.omit({ id: true })
