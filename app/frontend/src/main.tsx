2d84a40b85e544a0886c8418b5981d2e5a7477ee
Bo Tembunkiart <Aklaran@users.noreply.github.com>
2024-05-18 23:35:35 -0400

    feat: Barebones implement TanStack Router

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { httpBatchLink } from '@trpc/client'
+import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import './index.css'
+import { trpc } from './lib/trpc'
+
+// Import the generated route tree
+import { routeTree } from './routeTree.gen'
+
+// Create a new router instance
+const router = createRouter({ routeTree })
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+const queryClient = new QueryClient()
+
+const trpcClient = trpc.createClient({
+  // TODO: Use environment variable
+  links: [httpBatchLink({ url: 'http://localhost:5173/api' })],
+})
+
+// Render the app
+const rootElement = document.getElementById('root')!
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <trpc.Provider client={trpcClient} queryClient={queryClient}>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </trpc.Provider>
+    </StrictMode>,
+  )
+}
---
+++ app/frontend/src/routes/index.tsx
@@ -0,0 +1,20 @@
+import { Flashcard } from '@/components/custom-ui/flashcard'
+import { trpc } from '@/lib/trpc'
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: Index,
+})
+
+function Index() {
+  const flashcardQuery = trpc.flashcard.get.useQuery({ id: 1 })
+
+  return (
+    flashcardQuery.data && (
+      <Flashcard
+        question={flashcardQuery.data?.question}
+        answer={flashcardQuery.data?.answer}
+      />
+    )
+  )
+}
