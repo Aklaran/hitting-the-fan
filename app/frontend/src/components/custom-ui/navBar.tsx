62ba0303036d54cf4a36bc79fc4e1ed9631b3e73
Bo Tembunkiart <Aklaran@users.noreply.github.com>
2024-05-21 20:19:17 -0400

    refactor: Move NavBar to its own file

---
+++ app/frontend/src/components/custom-ui/navBar.tsx
@@ -0,0 +1,60 @@
+import Button from '@/components/ui/button'
+import { trpc } from '@/lib/trpc'
+import { Link } from '@tanstack/react-router'
+
+export const NavBar = () => {
+  return (
+    <div className="p-2 flex justify-between">
+      <div className="flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>{' '}
+        <Link to="/about" className="[&.active]:font-bold">
+          About
+        </Link>
+        <Link to="/flashcards" className="[&.active]:font-bold">
+          Flashcards
+        </Link>
+      </div>
+      <AccountActionNavSection />
+    </div>
+  )
+}
+function AccountActionNavSection() {
+  // TODO: Lol if only I had this in context. Can always give it another try!
+  const { data, isError, error } = trpc.user.isAuthenticated.useQuery()
+
+  if (isError) {
+    console.error('Error in AuthenticatedComponent', error)
+    return <UnauthenticatedNavSection />
+  }
+
+  if (!data) {
+    return <UnauthenticatedNavSection />
+  }
+
+  return <AuthenticatedNavSection />
+}
+function UnauthenticatedNavSection() {
+  return (
+    <div className="flex gap-2">
+      <Button asChild>
+        <a href="/api/auth/login" className="[&.active]:font-bold">
+          Login
+        </a>
+      </Button>
+      <Button asChild>
+        <a href="/api/auth/register" className="[&.active]:font-bold">
+          Register
+        </a>
+      </Button>
+    </div>
+  )
+}
+function AuthenticatedNavSection() {
+  return (
+    <Link to="/users/me" className="[&.active]:font-bold">
+      Profile
+    </Link>
+  )
+}
