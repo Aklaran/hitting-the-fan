93a21810beeb1edcb5640c4e8ac4e2fb5e8a07fe
Bo Tembunkiart <Aklaran@users.noreply.github.com>
2024-09-18 22:37:28 -0400

    refactor: Reuse schema checkers and ailment query

--- app/backend/src/engine/scenarioUtils.ts
+++ app/backend/src/engine/scenarioUtils.ts
@@ -1,4 +1,11 @@
-import { ScenarioLogEntry, ScenarioState } from '@shared/types/scenario'
+import {
+  Ailment,
+  BodyPart,
+  bodyPartSchema,
+  ScenarioLogEntry,
+  ScenarioState,
+} from '@shared/types/scenario'
+import { z, ZodTypeAny } from 'zod'
 
 const appendLogEntry = (
   scenarioState: ScenarioState,
@@ -14,6 +21,26 @@ const appendLogEntry = (
   return scenarioState
 }
 
+const isSchema = <T extends ZodTypeAny>(
+  schema: T,
+  obj: unknown,
+): obj is z.infer<T> => {
+  return schema.safeParse(obj).success
+}
+
+const isBodyPart = (obj: unknown): obj is BodyPart => {
+  return isSchema(bodyPartSchema, obj)
+}
+
+const getAilmentsByBodyPart = (ailments: Ailment[], bodyPart: BodyPart) => {
+  return ailments
+    .flatMap((ailment) => ailment.effects.bodyParts)
+    .filter((part) => part.part === bodyPart.part)
+}
+
 export const scenarioUtils = {
   appendLogEntry,
+  isBodyPart,
+  isSchema,
+  getAilmentsByBodyPart,
 }
--- app/backend/src/engine/verbHandlers/palpateHandler.ts
+++ app/backend/src/engine/verbHandlers/palpateHandler.ts
@@ -1,22 +1,14 @@
-import {
-  Ailment,
-  BodyPart,
-  bodyPartSchema,
-  Command,
-  ScenarioState,
-  VerbHandler,
-} from '@shared/types/scenario'
-import { z, ZodTypeAny } from 'zod'
+import { Command, ScenarioState, VerbHandler } from '@shared/types/scenario'
 import { scenarioUtils } from '../scenarioUtils'
 
 export const palpateHandler: VerbHandler = {
   execute: (command: Command, scenarioState: ScenarioState): ScenarioState => {
     let responseText = 'You paw at the air. It feels like air. (NO OBJECT)'
 
-    if (isBodyPart(command.object)) {
+    if (scenarioUtils.isBodyPart(command.object)) {
       responseText = command.object.palpationResponse
 
-      const ailments = getAilmentsByBodyPart(
+      const ailments = scenarioUtils.getAilmentsByBodyPart(
         scenarioState.patient.ailments,
         command.object,
       )
@@ -30,21 +22,4 @@ export const palpateHandler: VerbHandler = {
     scenarioUtils.appendLogEntry(scenarioState, responseText, 'narrator')
     return scenarioState
   },
-}
-
-const getAilmentsByBodyPart = (ailments: Ailment[], bodyPart: BodyPart) => {
-  return ailments
-    .flatMap((ailment) => ailment.effects.bodyParts)
-    .filter((part) => part.part === bodyPart.part)
-}
-
-const isBodyPart = (obj: unknown): obj is BodyPart => {
-  return isSchema(bodyPartSchema, obj)
-}
-
-export function isSchema<T extends ZodTypeAny>(
-  schema: T,
-  obj: unknown,
-): obj is z.infer<T> {
-  return schema.safeParse(obj).success
 }
