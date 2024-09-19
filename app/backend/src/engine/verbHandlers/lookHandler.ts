ad9ca249c10c4315d36158dde6f35543ac7a52e9
Bo Tembunkiart <Aklaran@users.noreply.github.com>
2024-09-18 22:38:14 -0400

    feat: See ailments when looking at body parts

--- app/backend/src/engine/verbHandlers/lookHandler.ts
+++ app/backend/src/engine/verbHandlers/lookHandler.ts
@@ -3,9 +3,23 @@ import { scenarioUtils } from '../scenarioUtils'
 
 export const lookHandler: VerbHandler = {
   execute: (command: Command, scenarioState: ScenarioState): ScenarioState => {
-    const responseText =
+    let responseText =
       command.object?.description ??
       'You stare vacantly into space. (NO OBJECT)'
+
+    if (scenarioUtils.isBodyPart(command.object)) {
+      responseText = command.object.description
+
+      const ailments = scenarioUtils.getAilmentsByBodyPart(
+        scenarioState.patient.ailments,
+        command.object,
+      )
+
+      responseText = [
+        responseText,
+        ...ailments.map((ailment) => ailment.description),
+      ].join(' ')
+    }
 
     scenarioUtils.appendLogEntry(scenarioState, responseText, 'narrator')
     return scenarioState
