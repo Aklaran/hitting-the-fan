63196561aa6fe8acf66afcbe56048cdb2c8d7fb4
Aklaran <Aklaran@users.noreply.github.com>
2025-10-20 22:45:18 -0400

    fix: Don't hardcode vite port for trpc
    Turns out you can use relative paths so it'll work in prod too

--- app/frontend/src/lib/trpc.ts
+++ app/frontend/src/lib/trpc.ts
@@ -4,6 +4,5 @@ import { createTRPCReact, httpBatchLink } from '@trpc/react-query'
 export const trpc = createTRPCReact<AppRouter>()
 
 export const trpcClient = trpc.createClient({
-  // TODO: Use environment variable
-  links: [httpBatchLink({ url: 'http://localhost:5173/api/trpc' })],
+  links: [httpBatchLink({ url: '/api/trpc' })],
 })
