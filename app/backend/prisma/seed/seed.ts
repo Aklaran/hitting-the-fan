f3849e86205e06053231912fcd327828782da514
Aklaran <Aklaran@users.noreply.github.com>
2025-11-15 00:13:12 -0500

    feat: Seed flashcards

--- app/backend/prisma/seed/seed.ts
+++ app/backend/prisma/seed/seed.ts
@@ -1,10 +1,39 @@
 import { PrismaClient } from '@prisma/client'
+import { flashcards } from './flashcards'
 import { scenarios } from './scenarios'
 
 const prisma = new PrismaClient()
 
 async function main() {
-  console.log('Seeding scenarios...')
+  console.log('Seeding flashcards...')
+  console.log(`Found ${flashcards.length} flashcards to seed.\n`)
+
+  let seededFlashcards = 0
+  for (const flashcard of flashcards) {
+    try {
+      await prisma.flashcard.upsert({
+        where: { key: flashcard.key },
+        create: flashcard,
+        update: flashcard,
+      })
+      seededFlashcards++
+      console.log(`Seeded flashcard ${flashcard.key}.`)
+    } catch (error) {
+      console.error(`Error seeding flashcard ${flashcard.key}: ${error}`)
+    }
+  }
+
+  const flashcardWord = seededFlashcards === 1 ? 'flashcard' : 'flashcards'
+  console.log(`\n${seededFlashcards} ${flashcardWord} seeded successfully.`)
+
+  if (seededFlashcards < flashcards.length) {
+    const failedCount = flashcards.length - seededFlashcards
+    const flashcardWord = failedCount === 1 ? 'flashcard' : 'flashcards'
+    throw new Error(`${failedCount} ${flashcardWord} failed to seed.`)
+  }
+
+  console.log('\nSeeding scenarios...')
+
   console.log(`Found ${scenarios.length} scenarios to seed.\n`)
 
   let seededScenarios = 0
